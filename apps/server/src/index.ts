ee767d0b1a745817c8ba13095fb847f52934af0c
abdul <up2dul@gmail.com>
2026-02-11 17:28:09 +0700

    feat(apps/server): add graphql to the entry point app

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,6 +2,8 @@ import { env } from "@ra-healthcare/env/server";
 import cors from "cors";
 import express from "express";
 
+import { yoga } from "@/graphql";
+
 const app = express();
 
 app.use(
@@ -17,6 +19,11 @@ app.get("/", (_req, res) => {
   res.status(200).send("OK");
 });
 
+app.use(yoga.graphqlEndpoint, yoga);
+
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
+  console.log(
+    `GraphQL playground at http://localhost:3000${yoga.graphqlEndpoint}`,
+  );
 });
