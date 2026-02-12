9e97d752c8080a2272891ce974edacb69a826f0f
abdul <up2dul@gmail.com>
2026-02-12 10:58:04 +0700

    refactor(apps/web/graphql): simplify workflow queries and mutations

--- apps/web/src/graphql/workflow.ts
+++ apps/web/src/graphql/workflow.ts
@@ -13,47 +13,17 @@ export const workflowStepFields = gql`
 export const workflowStepsQuery = gql`
   query WorkflowSteps {
     workflowSteps {
-      data {
-        ...WorkflowStepFields
-      }
-      total
-      page
-      limit
-      totalPages
-    }
-  }
-  ${workflowStepFields}
-`;
-
-export const workflowStepQuery = gql`
-  query WorkflowStep($id: ID!) {
-    workflowStep(id: $id) {
       ...WorkflowStepFields
     }
   }
   ${workflowStepFields}
 `;
 
-export const createWorkflowStepMutation = gql`
-  mutation CreateWorkflowStep($input: CreateWorkflowStepInput!) {
-    createWorkflowStep(input: $input) {
+export const saveWorkflowMutation = gql`
+  mutation SaveWorkflow($input: SaveWorkflowInput!) {
+    saveWorkflow(input: $input) {
       ...WorkflowStepFields
     }
   }
   ${workflowStepFields}
-`;
-
-export const updateWorkflowStepMutation = gql`
-  mutation UpdateWorkflowStep($id: ID!, $input: UpdateWorkflowStepInput!) {
-    updateWorkflowStep(id: $id, input: $input) {
-      ...WorkflowStepFields
-    }
-  }
-  ${workflowStepFields}
-`;
-
-export const deleteWorkflowStepMutation = gql`
-  mutation DeleteWorkflowStep($id: ID!) {
-    deleteWorkflowStep(id: $id)
-  }
 `;
